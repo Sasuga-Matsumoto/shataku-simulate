d1be317cbb3bdfca3a8487ddcb58fd43a7d643ef
unknown <matsumotosasuga@gmail.com>
2026-03-10 23:40:56 +0900

    Revert "Add Next.js shataku-lp with component-level A/B testing infrastructure"
    This reverts commit a5a2ebe20da5cb2bd1bc5bb17e298242dbc5de55.

--- output/shataku-lp/src/lib/get-variant.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers';
-import { getCookieName } from './ab-tests';
-
-export async function getVariant(testId: string): Promise<string> {
-  const cookieStore = await cookies();
-  const cookieName = getCookieName(testId);
-  const value = cookieStore.get(cookieName)?.value;
-  return value || 'redesign';
-}
